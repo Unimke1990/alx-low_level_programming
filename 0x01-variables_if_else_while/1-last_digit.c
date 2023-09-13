b1551b595ba28889442642599daba5ef158039f2
Unimke1990 <agimunimke1990@gmail.com>
2023-09-13 11:08:21 -0700

    finding the last digit of a number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,17 +19,17 @@ lastdigit = n % 10;
 
 if (lastdigit > 5)
 {
-printf("the last digit of %d is %d and is greater than 5\n", n, lastdigit);
+printf("last digit of %d is %d and is greater than 5\n", n, lastdigit);
 }
 
 else if (lastdigit == 0)
 {
-printf("the last digit of %d is %d and is 0\n", n, lastdigit);
+printf("last digit of %d is %d and is 0\n", n, lastdigit);
 }
 
 else
 {
-printf("the last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
+printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
 }
 return (0);
 }
