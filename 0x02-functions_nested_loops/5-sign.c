c06783df53c1fda99f57695e8939858ec03d01ac
Unimke1990 <agimunimke1990@gmail.com>
2023-09-16 09:11:59 -0700

    function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,15 +15,15 @@ _putchar('+');
 return (1);
 }
 
-else if (n == 1)
+else if (n == 0)
 {
-_putchar('0');
+_putchar(48);
 return (0);
 }
 
-else
+else if (n < 0)
 {
 _putchar('-');
-return (48);
 }
+return (-1);
 }
